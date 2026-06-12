edcf7792a1ddb231defd6bfbb26c1e2faabc68c9
JimStencil <jeancarlon7@hotmail.com>
2026-06-12 12:26:53 -0300

    fix: testes leitorCSV

--- src/test/java/br/edu/ufrgs/util/LeitorCSVTest.java
+++ src/test/java/br/edu/ufrgs/util/LeitorCSVTest.java
@@ -18,9 +18,12 @@ import static org.junit.jupiter.api.Assertions.*;
 class LeitorCSVTest {
     
     private Path tempFile;
+    private LeitorCSV leitor;  
     
     @BeforeEach
     void setUp() throws IOException {
+        leitor = new LeitorCSV();
+        
         tempFile = Files.createTempFile("config", ".csv");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile.toFile()))) {
             writer.write("nome_setor,fator_emissao,limite_mensal\n");
@@ -32,9 +35,10 @@ class LeitorCSVTest {
     
     @Test
     void testLerConsumoMaquinas() {
-        // eeste teste precisa do arquivo "consumo_maquinas_teste.csv" no diretório correto
+        // este teste precisa do arquivo "consumo_maquinas_teste.csv" no diretório correto
         String caminho = "src/test/resources/consumo_maquinas_teste.csv";
-        List<ConsumoMaquina> maquinas = LeitorCSV.lerConsumoMaquinas(caminho);
+        
+        List<ConsumoMaquina> maquinas = leitor.lerConsumoMaquinas(caminho);
         
         assertNotNull(maquinas);
         assertEquals(5, maquinas.size());
@@ -49,7 +53,7 @@ class LeitorCSVTest {
     
     @Test
     void testLerConfiguracoesSetores() {
-        Map<String, SetorConfiguracao> configs = LeitorCSV.lerConfiguracoesSetores(tempFile.toString());
+        Map<String, SetorConfiguracao> configs = leitor.lerConfiguracoesSetores(tempFile.toString());
         
         assertEquals(3, configs.size());
         assertEquals(0.85, configs.get("Usinagem").getFatorEmissao());
@@ -59,7 +63,7 @@ class LeitorCSVTest {
     
     @Test
     void testArquivoInexistente() {
-        Map<String, SetorConfiguracao> configs = LeitorCSV.lerConfiguracoesSetores("arquivo_que_nao_existe.csv");
+        Map<String, SetorConfiguracao> configs = leitor.lerConfiguracoesSetores("arquivo_que_nao_existe.csv");
         
         assertNotNull(configs);
         assertTrue(configs.isEmpty());
